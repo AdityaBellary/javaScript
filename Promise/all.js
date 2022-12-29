86c66fba3d130998d8b771353a6fc145e71a92d9
Aditya Bellary <adityabellary71@gmail.com>
2022-12-29 20:27:46 +0530

    promise-all

---
+++ Promise/all.js
@@ -0,0 +1,6 @@
+/*
+The Promise.all() accepts an array of promises and returns a promise
+that resolves when all of the promises in the array are fulfilled or when
+the iterable contains no promises. It rejects with the reason of the first
+promise that rejects.
+*/
