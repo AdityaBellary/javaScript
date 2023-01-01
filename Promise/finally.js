c20d7ef343848f9ea90a6faccb4bf1797e6ee8b8
Aditya Bellary <adityabellary71@gmail.com>
2023-01-01 21:39:51 +0530

    promises

---
+++ Promise/finally.js
@@ -0,0 +1,6 @@
+/*
+1. Finally is something which is scheduled once the promise is settled irrespective 
+    of it being resolved or rejected.
+2. Same as try, catch has finally.. similarly promise has (resolve, reject, finally)   
+*/
+
