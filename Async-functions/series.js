fcc2a5d731648f82c3707529eef7d5c77613e950
Aditya Bellary <adityabellary71@gmail.com>
2023-01-10 22:11:23 +0530

    async-series

--- Async-functions/series.js
+++ Async-functions/series.js
@@ -1,1 +1,47 @@
 // Execute async functions in Series
+
+// using async-await and a for loop
+const asyncSeriesExecute = async (promises) => {
+    for (let promise of promises){
+        try{
+            const result = await promise
+            console.log(result)
+        }catch(e){
+            console.log(e);
+        }
+    }
+}
+
+// using recursion
+const asyncSeriesExecuter = (promises) => {
+    const promise = promises.shift()
+    promise.then((data) => {
+        console.log(data)
+        if (promises.length > 0){
+            asyncSeriesExecuter(promises)
+        }
+    })
+}
+
+// using reduce
+const asyncSeriesExecuters = function(promises) {
+    promises.reduce((acc, curr) => {
+    return acc.then(() => {
+    return curr.then(val => {console.log(val)});
+    });
+    }, Promise.resolve());
+    }
+
+const asyncTask = function(i) {
+return new Promise((resolve, reject) => {
+setTimeout(() => resolve(`Completing ${i}`), 100*i)
+});
+}
+const promises = [
+asyncTask(3),
+asyncTask(1),
+asyncTask(7),
+asyncTask(2),
+asyncTask(5),
+];
+asyncSeriesExecute(promises);
