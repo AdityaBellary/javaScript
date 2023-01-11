94aaa1f81b0ccfb54e6aad6950cebb9ee4b2535b
Aditya Bellary <adityabellary71@gmail.com>
2023-01-11 21:48:03 +0530

    async-parallel

--- Async-functions/parallel.js
+++ Async-functions/parallel.js
@@ -1,2 +1,38 @@
 // Execute async functions in parallel
-// clean function
+
+function asyncParallel(tasks, callback) {
+    const results = []
+    let tasksCompleted = 0
+    tasks.forEach(asyncTask => {
+        asyncTask(value => {
+            results.push(value)
+            tasksCompleted++
+            if (tasksCompleted >= tasks.length) {
+                callback(results)
+            }
+        })
+    })
+}
+
+function createAsyncTask() {
+    const value = Math.floor(Math.random() * 10)
+    console.log(20, value)
+    return function (callback) {
+        setTimeout(() => {
+            callback(value)
+        }, value * 1000)
+    }
+}
+
+const taskList = [createAsyncTask(),
+createAsyncTask(),
+createAsyncTask(),
+createAsyncTask(),
+createAsyncTask(),
+createAsyncTask(),
+createAsyncTask(),
+createAsyncTask()]
+
+asyncParallel(taskList, result => {
+    console.log('results', result);
+});
