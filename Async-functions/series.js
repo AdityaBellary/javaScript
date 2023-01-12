8a9c85fae37510af784d0bb877bf4dada193c425
Aditya Bellary <adityabellary71@gmail.com>
2023-01-12 21:44:38 +0530

    series format

--- Async-functions/series.js
+++ Async-functions/series.js
@@ -2,11 +2,11 @@
 
 // using async-await and a for loop
 const asyncSeriesExecute = async (promises) => {
-    for (let promise of promises){
-        try{
+    for (let promise of promises) {
+        try {
             const result = await promise
             console.log(result)
-        }catch(e){
+        } catch (e) {
             console.log(e);
         }
     }
@@ -17,31 +17,31 @@ const asyncSeriesExecuter = (promises) => {
     const promise = promises.shift()
     promise.then((data) => {
         console.log(data)
-        if (promises.length > 0){
+        if (promises.length > 0) {
             asyncSeriesExecuter(promises)
         }
     })
 }
 
 // using reduce
-const asyncSeriesExecuters = function(promises) {
+const asyncSeriesExecuters = function (promises) {
     promises.reduce((acc, curr) => {
-    return acc.then(() => {
-    return curr.then(val => {console.log(val)});
-    });
+        return acc.then(() => {
+            return curr.then(val => { console.log(val) });
+        });
     }, Promise.resolve());
-    }
+}
 
-const asyncTask = function(i) {
-return new Promise((resolve, reject) => {
-setTimeout(() => resolve(`Completing ${i}`), 100*i)
-});
+const asyncTask = function (i) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`Completing ${i}`), 100 * i)
+    });
 }
 const promises = [
-asyncTask(3),
-asyncTask(1),
-asyncTask(7),
-asyncTask(2),
-asyncTask(5),
+    asyncTask(3),
+    asyncTask(1),
+    asyncTask(7),
+    asyncTask(2),
+    asyncTask(5),
 ];
 asyncSeriesExecute(promises);
